0d171c7291be4c0759696f1d1610cfb1c7f4a643
Gabriel Feceoru <gabi.feceoru@gmail.com>
2025-12-14 17:22:41 +0100

    Locate executable files

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,42 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::env;
+use std::fs;
+use std::os::unix::fs::PermissionsExt;
 
-const SHELL_BUILTINS: [&str; 3] = ["exit", "echo", "type"];
+const SHELL_BUILTINS: &[&str] = &["exit", "echo", "type"];
+
+fn is_executable(path: &std::path::Path) -> bool {
+    if let Ok(metadata) = fs::metadata(path) {
+       return metadata.permissions().mode() & 0o111 != 0;
+    }
+
+    false
+}
+
+fn find_in_path(command: &str) -> Option<String> {
+    let Some(path_os) = env::var_os("PATH") else {
+        return None;
+    };
+
+    for dir in env::split_paths(&path_os) {
+        let candidate = dir.join(command);
+
+        // If the file exists but lacks execute permissions, skip it and continue.
+        if candidate.exists() && !is_executable(&candidate) {
+            continue;
+        }
+
+        if is_executable(&candidate) {
+            return Some(candidate.to_string_lossy().into_owned());
+        }
+    }
+
+    None
+}
 
 fn main() {
-    while true {
+    loop {
         print!("$ ");
         io::stdout().flush().unwrap();
 
@@ -16,10 +48,18 @@ fn main() {
         match cmd[0] {
             "exit" => break,
             "echo" => println!("{}", args.join(" ") ),
-            "type" => if SHELL_BUILTINS.contains(&cmd[1])  {
-                println!("{} is a shell builtin", cmd[1]);
-            } else {
-                println!("{}: not found", cmd[1]);
+            "type" => {
+                let Some(query) = cmd.get(1).copied() else {
+                    continue;
+                };
+
+                if SHELL_BUILTINS.contains(&query)  {
+                    println!("{} is a shell builtin", &query);
+                } else if let Some(full_path) = find_in_path(query) {
+                    println!("{} is {}", query, full_path);
+                } else {
+                    println!("{}: not found", query);
+                }
             },
             _ => println!("{}: command not found", command.trim()),
         }
